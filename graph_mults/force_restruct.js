ee922302e70a485c1083ba0c8e9007987a6f6d0b
Michael Hirsch <michael.hirsch@rga.com>
2016-01-25 17:08:23 -0500

    global scale

--- graph_mults/force_restruct.js
+++ graph_mults/force_restruct.js
@@ -1,52 +1,51 @@
-// load data first separately
+//globals
+var fillScale = undefined;
 
-// make call to countries API to get full list of countries. this allows to set
-// color scale before we create the graphs
+function init(){
+    //load countries data
+    d3.json("http://localhost:5000/countries", function(json){
+        countries = json.countries;
+        buildFillScale(countries)
+    })
+    //build Graphs
+}
 
-// var color = function() {
-//
-//     var data;
-//
-//     return d3.json("http://localhost:5000/countries", GetFill);
-//
-//     function GetFill(jsonData){
-//
-//         // this.fill = d3.scale.category20()
-//         //     .domain(jsonData.countries);
-//
-//         data = jsonData.countries;
-//         return data;
-//     }
-//
-// };
+function buildFillScale(countries){
+    fillScale = d3.scale.category10().domain(countries);
+    buildGraphs()
+}
 
-    // function(json){
-    //     fill = d3.scale.category20()
-    //             .domain(json.countries);
-    //     setFill();
-    //         })
-    //
-    // function setFill(){
-    //     var obj = {
-    //         fill: fill
-    //     };
-    //     return obj;
-    //     }
-    // return setFill();
-    // };
-    //
-    // return getit()
+function buildGraphs(){
+    var graph2 = new Graph("Games",1);
+    var graph3 = new Graph("SaaS",1);
+    var graph4 = new Graph("News",1);
+    var graph5 = new Graph("Education Tech",1);
+    var graph6 = new Graph("Software",1);
+    var graph7 = new Graph("Fashion",1);
+    var graph7 = new Graph("Digital Media",1);
+    var graph7 = new Graph("Restaurants and food",1);
+    var graph7 = new Graph("Web Design",1);
+    var graph7 = new Graph("Financial Services",1);
+    var graph7 = new Graph("Enterprise Software",1);
+    var graph7 = new Graph("Social Media",1);
+    var graph7 = new Graph("Transportation",1);
+    var graph7 = new Graph("Social Networking",1);
+    var graph7 = new Graph("Human Resource",1);
+    var graph7 = new Graph("Shopping",1);
+    var graph7 = new Graph("Services",1);
+    var graph7 = new Graph("Health Care",1);
+}
 
 var Graph = function(industry, subgraphs) {
 
-var w = 300,
-    h = 300,
-    fill = d3.scale.category10();
+var w = 250,
+    h = 250
+
 
 var vis = d3.select("body")
   .append("div")
   .style("float","left")
-  .attr("id", industry.replace(" ","_"))
+  .attr("id", industry.replace(/ /g,"_"))
   .append("svg:svg")
   .attr("width", w)
   .attr("height", h);
@@ -56,11 +55,11 @@ var file = "http://localhost:5000/industryGraph/" + industry + "?subgraphs=" + s
 d3.json(file, function(json) {
 
   var node_scale = d3.scale.linear()
-                    .domain([1, d3.max(json.nodes, function(d) { return d["degree"]; })])
-                    .range([2,9]);
-
+                    .domain([0, d3.max(json.nodes, function(d) { return d["cent_deg"]; })])
+                    .range([3,10]);
+    console.log(node_scale.domain())
   var force = d3.layout.force()
-      .charge(-10)
+      .charge(-5)
       .linkDistance(15)
       .nodes(json.nodes)
       .links(json.links)
@@ -71,7 +70,8 @@ d3.json(file, function(json) {
       .data(json.links)
     .enter().append("svg:line")
       .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); })
+      .style("stroke-width", 3)
+      .style("stroke", "black")
       .attr("x1", function(d) { return d.source.x; })
       .attr("y1", function(d) { return d.source.y; })
       .attr("x2", function(d) { return d.target.x; })
@@ -84,9 +84,10 @@ d3.json(file, function(json) {
       .attr("class", function(d){ return d.group})
       .attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; })
-      .attr("r", function(d) {return node_scale(d.degree)})
+      .attr("r", function(d) {return node_scale(d.cent_deg)})
       .attr("country", function(d){return d.location;})
-      .style("fill", function(d) { return fill(d.location); })
+      .style("fill", function(d) { return fillScale(d.location); })
+      .style("stroke", function(d){return d.group == 1 ? "black" : undefined })
       .call(force.drag);
 
   node.append("svg:title")
@@ -98,7 +99,7 @@ d3.json(file, function(json) {
       .style("opacity", 1);
 
 
-    d3.select("#"+industry.replace(" ","_"))
+    d3.select("#"+industry.replace(/ /g,"_"))
     .append("div")
     .text(industry)
     .style("text-align", "center");
@@ -117,11 +118,4 @@ d3.json(file, function(json) {
 
 };
 
-var pooofill = d3.scale.category20().domain();
-var graph2 = new Graph("Games",2);
-var graph3 = new Graph("SaaS",2);
-var graph4 = new Graph("News",2);
-var graph5 = new Graph("Education Tech",2);
-var graph6 = new Graph("Software",2);
-var graph7 = new Graph("Fashion",2);
-var graph7 = new Graph("Services",2);
+init()
